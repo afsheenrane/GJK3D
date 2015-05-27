8ceafbe6aba5e2a63952d02d3298d8614c6ba1e3
afsheen <afsheen_rane@hotmail.com>
2015-05-27 13:58:57 -0400

    Added some documentation and new methods (cross, dot).

--- src/gjk3d/Vec3D.java
+++ src/gjk3d/Vec3D.java
@@ -24,15 +24,28 @@ public class Vec3D {
         this.y = y;
         this.z = z;
     }
-
+    
+    /**
+     * Get the length of the vector.
+     * @return the scalar length of this vector.
+     */
     public double getLength(){
         return Math.sqrt(getSquaredLength());
     }
 
+    /**
+     * Get the squared length of the vector. Useful for cheap vector 
+     * comparisons. 
+     * @return the scalar squared length of this vector.
+     */
     public double getSquaredLength(){
         return (x * x) + (y * y) + (z * z);
     }
-
+    
+    /**
+     * Get a normalized copy of this vector. 
+     * @return a normalized copy of this vector.
+     */
     public Vec3D getNormalized(){
         double length = getLength();
         length = (length == 0 ? 1 : length);
@@ -40,31 +53,90 @@ public class Vec3D {
         return new Vec3D(x / length, y / length, z / length);
     }
 
+    /**
+     * Get the dot product of v1 and v2.
+     * @param v1 the first vector.
+     * @param v2 the second vector.
+     * @return the scalar dot product of v1 and v2.
+     */
+    public static double dot(Vec3D v1, Vec3D v2){
+    	return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+    }
+        
+    /**
+     * The dot product of this vector to the other vector v.
+     * @param v the vector to dot product with.
+     * @return the dot product of this vector and v.
+     */
     public double dot(Vec3D v){
-        return (x * v.x) + (y * v.y) + (z * v.z);
+        return Vec3D.dot(this, v);
+    }
+    
+    /**
+     * Get the cross product of vectors v1 and v2.
+     * @param v1 the first vector.
+     * @param v2 the second vector.
+     * @return the vector cross product of v1 and v2.
+     */
+    public static Vec3D cross(Vec3D v1, Vec3D v2){
+    	double x, y, z;
+    	x = (v1.y * v2.getZ()) - (v1.z * v2.getY());
+    	y = (v1.z * v2.getX()) - (v1.x * v2.getZ());
+    	z = (v1.x * v2.getY()) - (v1.y * v2.getX());
+    	
+    	return new Vec3D(x, y, z);
+    }
+    
+    /**
+     * Get the cross product of this vector and v.
+     * @param v the vector to cross product with.
+     * @return the cross product vector of this and v.
+     */
+    public Vec3D cross(Vec3D v){
+    	return Vec3D.cross(this, v);
     }
 
-    public double getX() {
-        return x;
-    }
+	/**
+	 * @return the x
+	 */
+	public double getX() {
+		return x;
+	}
 
-    public void setX(double x) {
-        this.x = x;
-    }
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(double x) {
+		this.x = x;
+	}
 
-    public double getY() {
-        return y;
-    }
+	/**
+	 * @return the y
+	 */
+	public double getY() {
+		return y;
+	}
 
-    public void setY(double y) {
-        this.y = y;
-    }
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(double y) {
+		this.y = y;
+	}
 
-    public double getZ() {
-        return z;
-    }
+	/**
+	 * @return the z
+	 */
+	public double getZ() {
+		return z;
+	}
 
-    public void setZ(double z) {
-        this.z = z;
-    }
+	/**
+	 * @param z the z to set
+	 */
+	public void setZ(double z) {
+		this.z = z;
+	}
+    
+    
 }
